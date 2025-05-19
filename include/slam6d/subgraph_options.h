caf3d0605c57ed150293a186a3a8c1e8a475ecf4
Fabian Arzberger <fabian.arzberger@uni-wuerzburg.de>
2025-05-19 15:45:49 +0200

    Adding subgraph icp from addons

---
+++ include/slam6d/subgraph_options.h
@@ -0,0 +1,155 @@
+#ifndef SGICP_OPTIONS
+#define SGICP_OPTIONS
+
+#include <string>
+#include <boost/program_options.hpp>
+#include "slam6d/io_types.h"
+
+namespace po = boost::program_options;
+
+// Boost needs to convert from string to IOType. Use this validation function.
+void validate(boost::any& v, const std::vector<std::string>& values,
+              IOType*, int) {
+  if (values.size() == 0)
+    throw std::runtime_error("Invalid model specification");
+  std::string arg = values.at(0);
+  try {
+    v = formatname_to_io_type(arg.c_str());
+  } catch (...) { // runtime_error
+    throw std::runtime_error("Format " + arg + " unknown.");
+  }
+}
+
+int parse_options(  
+    int argc, 
+    char** argv, 
+    std::string &scandir, 
+    int& start,
+    int& end,
+    IOType &type, 
+    bool &quiet, 
+    int &maxDist, 
+    int &minDist, 
+    int &octree, 
+    double &red,
+    bool &scanserver,
+    bool &use_frames,
+    double &mdm,
+    double &mdml,
+    int& iter,
+    int& iter_lum,
+    double& epsICP,
+    double& epsSLAM,
+    int& subsize,
+    int& clpairs,
+    bool& meta,
+    int& max_num_metascans,
+    bool& icp_only
+    )
+{
+    // Add program option descriptions and reference variables accordingly
+    po::options_description generic("Generic options");
+    po::options_description input("Program options");
+    po::options_description reduction("Reduction options");
+    po::options_description matching("Matching options");
+    po::options_description hidden("Positional arguments");
+
+    // Add program options in a curried way, using the overloaded ()-operator
+    generic.add_options()
+        ("help,h", "Display a very helpful message")
+        ("quiet,q", po::bool_switch(&quiet)->default_value(false),
+            "Supress any output (except segfault) of the program.");
+    input.add_options()
+        ("format,f", po::value<IOType>(&type)->default_value(UOS, "uos"),
+            "chose input format from {uos, uosr, uos_rgb, ous_normal, old, xyz}")
+        ("start,s", po::value<int>(&start)->default_value(0),
+            "Skip the first <arg> scans.")
+        ("end,e", po::value<int>(&end)->default_value(-1),
+            "Stop at scan index <arg>")
+        ("scanserver", po::value<bool>(&scanserver)->default_value(false),
+            "Use the scanserver as an input method and handling of scan data")
+        ("continue", po::bool_switch(&use_frames)->default_value(false),
+            "Use pose specified in .frames file instead of .pose file.");
+    reduction.add_options()    
+        ("max,m", po::value<int>(&maxDist)->default_value(-1),
+            "neglegt all data points with a distance larger than <arg> 'units'")
+        ("min,M", po::value<int>(&minDist)->default_value(-1),
+            "neglegt all data points with a distance smaller than <arg> 'units'")
+        ("reduce,r", po::value<double>(&red)->default_value(-1.0),
+            "turns on octree based point reduction (voxel size= <arg>)")
+        ("octree,O", po::value<int>(&octree)->default_value(0),
+            "Use randomized octree based point reduction (pts per voxel=<arg>)");
+    matching.add_options()
+        ("dist,d", po::value<double>(&mdm)->default_value(25.0),
+            "Minimum distance for considering valid correspondence for ICP") 
+        ("distSLAM,D", po::value<double>(&mdml)->default_value(5.0),
+            "Minimum distance for considering valid correspondence for GraphSLAM")
+        ("iter,i", po::value<int>(&iter)->default_value(100),
+            "Maximum number of iterations.")
+        ("iterSLAM,I", po::value<int>(&iter_lum)->default_value(50),
+            "sets the maximal number of iterations for SLAM to <NR>"
+            "(if not set, graphSLAM is not executed)")
+        ("epsICP,E", po::value<double>(&epsICP)->default_value(0.00001),
+            "Stop ICP matching if difference is smaller than <arg>")
+        ("epsSLAM", po::value<double>(&epsSLAM)->default_value(0.05),
+            "Stop Graph SLAM if difference is smaller than <arg>")
+        ("clpairs,c", po::value<int>(&clpairs)->default_value(20),
+            "Consider an edge in the graph if more than <arg> point pairs exist")
+        ("size,S", po::value<int>(&subsize)->default_value(5),
+            "Number of scans in the subgraphs.")
+        ("metascan,2", po::bool_switch(&meta)->default_value(false),
+            "Match current scan against a meta scan of all previous scans (default match against the last scan only)")
+        ("maxmeta", po::value<int>(&max_num_metascans)->default_value(-1),
+            "maximum nr of previous scans to combine to a metascan in scan matching. The total number will be <arg>*<size>.")
+        ("icp", po::bool_switch(&icp_only)->default_value(false),
+            "Subgraphs perform ICP matching, no graphSLAM");
+    hidden.add_options()
+        ("input-dir", po::value<string>(&scandir), "input dir");
+
+    // All options together
+    po::options_description alloptions;
+    alloptions.add(generic).add(input).add(reduction).add(matching).add(hidden);
+
+    // Only commandline, visible with --help
+    po::options_description cmdoptions;
+    cmdoptions.add(generic).add(input).add(reduction).add(matching);
+
+    // positional argument for input directory 
+    po::positional_options_description pos;
+    pos.add("input-dir", 1); // max 1 pos arg
+
+    // Map and store option inputs to variables
+    po::variables_map vars;
+    po::store( po::command_line_parser(argc, argv).
+                options(alloptions).
+                positional(pos).
+                run(), 
+                vars);
+
+    // help display msg
+    if ( vars.count("help") )
+    {
+        cout << cmdoptions;
+        cout << endl;
+        cout << "This is an experimental program which performs scan registration." << endl;
+        cout << "The program puts S (--size) subsequent scans into a Graph." << endl;
+        cout << "First, GraphSLAM is performed on these so called \"Sub-Graphs\" of size S." << endl;
+        cout << "GraphSLAM or plain ICP is then performed on the subsequent Sub-Graphs." << endl;
+        cout << endl << "Example usage:" << endl 
+            << "bin/sgicp dat/yourscans -f uosr -r 10 -O 1 -d 50 -S 5" 
+            << endl;
+        exit(0);
+    }
+    po::notify(vars);
+
+    // Add trailing directory slash if there is none. Works differently when compiling under Windows
+#ifndef _MSC_VER
+    if (scandir[ scandir.length()-1 ] != '/') scandir = scandir + "/";
+#else
+    if (scandir[ scandir.length()-1]  != '\\') scandir = scandir + "\\";
+#endif
+
+    return 0;
+}
+
+#endif // SGICP_OPTIONS
---
+++ src/slam6d/subgraphicp.cc
@@ -0,0 +1,228 @@
+/** 
+ * subgraphicp implementation
+ * 
+ * The program puts S (--size) subsequent scans into a Graph.
+ * First, GraphSLAM is performed on these so called "Sub-Graphs" of size S.
+ * Then, another Graph is constructed where each Sub-Graph is connected to other Sub-Graphs.
+ * GraphSLAM or plain ICP is then performed on the subsequent Sub-Graphs.
+ * 
+ * @file 
+ * @brief An experimental program which performs scan registration. 
+ * 
+ * Motivation:
+ * This program plays with the inheritance of the MetaScan class and its base Scan class.
+ * The program is not intended to compete with srr (SemiRigidRegistration / correction).
+ * While this program potentially runs faster, srr is a much more powerfull tool. 
+ * My personal use-case for this program is as a fast and robust pre-registration step to srr.
+ * The Scan class defines the
+ * 
+ *  virtual DataPointer get(const std::string &identifier)
+ * 
+ * and
+ * 
+ *  virtual DataPointer create(const std::string& identifier, size_t size)
+ * 
+ * functions which MetaScan has to implement.
+ * I've made an attempt in include/slam6d/metaScan.h 
+ * There might be better ways to do that. 
+ * However, it allows us to write programs like this one,
+ * without breaking MetaScan's previous functionality. 
+ * 
+ * Example usage of extended MetaScan, essentially you can now do:
+ * 
+ *  // Create a vector of some scans (previously possible):
+ *  
+ *  ScanVector* someScans = new ScanVector();
+ *  someScans->push_back( Scan::allScans[0] );
+ *  someScans->push_back( Scan::allScans[1] ); 
+ *  someScans->push_back( Scan::allScans[2] ); // push any number of Scan*
+ *  
+ *  // Create a vector of some MetaScans (previously possible but useless):
+ *  
+ *  ScanVector* someMetaScans = new ScanVector();
+ *  someMetaScans->push_back( new MetaScan(someScans) );
+ *  someMetaScans->push_back( new MetaScan(someMoreScans) ); // push any number of MetaScan*
+ *  
+ *  // Perform GraphSLAM or ICP on MetaScans (previously impossible):
+ *  
+ *  // [...] (--> code to initialize graphSlam6D* gSlam and Graph* gr, see e.g. this file)
+ *  gSlam->doGraphSlam6D(*gr, *someMetaScans, iter);
+ * 
+ * @author Fabian Arzberger, Institute of CS, University of Wuerzburg, Germany  
+ *
+ */
+
+// System libs
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sstream>
+#include <iostream>
+
+// 3dtk internal
+#include "slam6d/point_type.h"
+#include "slam6d/globals.icc"
+#include "slam6d/metaScan.h"
+#include "slam6d/icp6Dquat.h"
+#include "slam6d/icp6D.h"
+#include "slam6d/Boctree.h"
+#include "slam6d/lum6DeulerS.h"
+#include "slam6d/subgraph_options.h"
+
+using std::cout;
+using std::endl;
+
+// Writes one transformation (mat) to the file
+inline void writeTransformToFrameFile(std::ofstream &file, const double* mat, unsigned int type)
+{
+  for (int i = 0; i < 16; ++i){
+    file << mat[i] << " "; 
+  } 
+  file << type << "\n" << std::flush;
+}
+
+// saves only the final transformation but NOT an animation
+inline void saveLastFrame(ScanVector *scans, bool append)
+{
+  // Save frames
+  for(size_t i = 0; i < scans->size(); ++i) {
+    Scan* scan = scans->at(i); 
+    const double *last_matrix;
+    Scan::AlgoType last_atype;
+    scan->getFrame(scan->getFrameCount() - 1, last_matrix, last_atype);
+    std::string filename = scan->getPath() + "scan" + scan->getIdentifier() + ".frames";
+    std::ios_base::openmode mode = append ? std::ios_base::app : std::ios_base::out;
+    std::ofstream file(filename.c_str(), mode);
+    writeTransformToFrameFile(file, last_matrix, last_atype);
+    file.close();
+  } 
+}
+
+//  Handling Segmentation faults and CTRL-C
+void sigSEGVhandler (int v)
+{
+  static bool segfault = false;
+  if(!segfault) {
+    segfault = true;
+    cout << endl
+      << "# **************************** #" << endl
+      << "  Segmentation fault or Ctrl-C" << endl
+      << "# **************************** #" << endl
+      << endl
+      << "Saving Frames... ";
+    saveLastFrame(&Scan::allScans, true);
+    cout << " done!" << endl;
+  }
+  exit(-1);
+}
+
+int main(int argc, char** argv) 
+{   
+  // Signal handling (CTRL+C or segfault)
+  signal (SIGSEGV, sigSEGVhandler);
+  signal (SIGINT,  sigSEGVhandler);
+  
+  // Parse program options
+  std::string scandir;
+  int start, end, maxdist, mindist, octree, iter, iter_lum, size, clpairs, maxmeta;
+  IOType type;
+  bool quiet, scanserver, use_frames, meta, icp_only;
+  double red, mdm, mdml, epsICP, epsSLAM;
+  parse_options(argc, argv, scandir, start, end, type, quiet, 
+    maxdist, mindist, octree, red, scanserver, use_frames, mdm, 
+    mdml, iter, iter_lum, epsICP, epsSLAM, size, clpairs, meta, maxmeta, icp_only);
+
+  // Read the scan directory
+  if (use_frames) 
+    Scan::continueProcessing();
+
+  Scan::setProcessingCommand(argc, argv);
+  Scan::openDirectory(scanserver, scandir, type, start, end);
+
+  // Create the subgraphs
+  vector<ScanVector*> subgraphs;
+  ScanVector* subgraph = new ScanVector();
+  for(size_t i = 0; i < Scan::allScans.size(); ++i) 
+  {
+    Scan* scan = Scan::allScans[i];
+    scan->setRangeFilter(maxdist, mindist);
+    scan->setReductionParameter(red, octree);
+    scan->setSearchTreeParameter(nns_type::BOCTree);
+    scan->calcReducedPoints();  
+    subgraph->push_back(scan);
+    if (subgraph->size() >= size)
+    {
+      cout << "Reduced points for segment " << subgraph->front()->getIdentifier() 
+        << "-" << subgraph->back()->getIdentifier() << endl; 
+      subgraphs.push_back(subgraph);
+      subgraph = new ScanVector();
+    }
+    double id[16];
+    M4identity(id);
+    scan->transform(id, Scan::ICP, 1 );
+    scan->transform(id, Scan::ICP, 1 );
+  } 
+  saveLastFrame(&Scan::allScans, false);
+
+  // Setup ICP minimizer and graph SLAM objects
+  icp6Dminimizer *icp_minimizer_functor = new icp6D_QUAT(); 
+  graphSlam6D *gSlam = new lum6DEuler(icp_minimizer_functor, mdml, mdml, iter_lum, 
+    true, false, 1, true, -1, epsICP, BOCTree, epsSLAM);
+  gSlam->set_quiet(true);
+
+  // Optimize subgraphs
+  for(size_t i = 0; i < subgraphs.size(); ++i) 
+  {
+    ScanVector* sg = subgraphs[i];
+    cout << "Optimizing graph between " << sg->front()->getIdentifier() 
+      << "-" << sg->back()->getIdentifier() << endl; 
+    Graph *gr = gSlam->computeGraph6Dautomatic(*sg, clpairs);
+    gSlam->doGraphSlam6D(*gr, *sg, iter_lum);
+  } 
+  saveLastFrame(&Scan::allScans, true);
+
+  // Create bundled scan packages , i.e. one Scan object per subgraph
+  cout << "Creating metascans... ";
+  ScanVector *metascans = new ScanVector();
+  metascans->reserve(subgraphs.size());
+  for(size_t i = 0; i < subgraphs.size(); ++i) 
+  {
+    // Get each subgraph
+    ScanVector *subgraph = subgraphs[i];
+    Scan *scan = new MetaScan(*subgraph);
+    scan->setRangeFilter(maxdist, mindist);
+    scan->setReductionParameter(red, octree);
+    scan->setSearchTreeParameter(nns_type::BOCTree, 20);
+    scan->calcReducedPoints();
+    scan->createSearchTree();
+    metascans->push_back(scan);
+  } 
+  
+  if (icp_only) 
+  {
+    // Do ICP on the metascans
+    icp6D *icp = new icp6D(icp_minimizer_functor, mdm, iter, 
+      false, meta, 1, true, 1, epsICP, nns_type::BOCTree, false, false, maxmeta);
+    icp->doICP(*metascans, PairingMode::CLOSEST_POINT);
+  } 
+  else 
+  {
+    // Do GraphSLAM on the metascans
+    delete icp_minimizer_functor;
+    icp_minimizer_functor = new icp6D_QUAT();
+    gSlam = new lum6DEuler(icp_minimizer_functor, mdm, mdm, iter, 
+      true, false, 1, true, -1, epsICP, BOCTree, epsSLAM);
+    gSlam->set_quiet(true);
+    Graph *gr = gSlam->computeGraph6Dautomatic(*metascans, clpairs);
+    gSlam->doGraphSlam6D(*gr, *metascans, iter);
+  }
+  // Metascans store the transformation on the metascan objects, 
+  // we have to apply them manually on the individual scans
+  double id[16];
+  M4identity(id);
+  for (size_t i = 0; i < metascans->size(); ++i)
+    metascans->at(i)->transform(id, Scan::ICP, 0);  // write end pose 
+
+  // Save frames (TODO: export animation as well)
+  saveLastFrame(&Scan::allScans, true);
+} 
