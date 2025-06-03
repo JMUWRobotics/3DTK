5818423c27255d06c5184fbfb1a1057b7e37a8c7
yannik <yannik@Softwarepraktikum.myguest.virtualbox.org>
2025-06-03 13:51:35 +0200

    issue #20 and undo of commit 6c45cb9

---
+++ src/sc_fixed/sc_fixed_math.cc
@@ -0,0 +1,29 @@
+#include "sc_fixed/sc_fixed_math.h"
+
+
+f_float sc_fixed_heron_sqrt(f_float s) {
+
+  f_float x = (s+1)/2;
+  f_float x_n;
+  
+  for(int i = HERON_ITERATIONS; i > 0; i--) {
+    x_n = (x + s / x) / 2;
+    x = x_n;
+  }
+
+  return x_n;
+  
+}
+
+// For testing
+int sc_main(int argc, char* argv[]){
+
+  f_float s = 163;
+  f_float res = sc_fixed_heron_sqrt(s);
+
+  cout << res << endl;
+  
+  return 0;
+
+}
+// END testing
