9213f83c5e2d4d92a67897f070bcaca0128e2996
trunks331 <95592289+trunks331@users.noreply.github.com>
2025-07-02 17:48:10 +0200

    Update sc_fixed_math.h
    Methoden sc_choldc und sc_cholsl hinzugefügt

--- include/sc_fixed/sc_fixed_math.h
+++ include/sc_fixed/sc_fixed_math.h
@@ -14,5 +14,7 @@ using namespace sc_dt;
 using f_float = sc_fixed<WORD_LENGTH, INT_WORD_LENGTH>;
 
 f_float sc_fixed_heron_sqrt(f_float s);
+bool sc_choldc(f_float A[3][3], f_float diag[3]);
+void sc_cholsl(f_float A[3][3], f_float diag[3], f_float B[3], f_float x[3]);
 
 #endif // SC_FIXED_MATH_H
