7cb38c1875ffa26b5210694070557f55ae8702d2
Fabia Frank <fabia.frank@stud-mail.uni-wuerzburg.de>
2026-05-11 12:40:59 +0200

    Fix glad-configuration failure

--- src/slam6d/fbr/new_click_tool/MVP/main.cpp
+++ src/slam6d/fbr/new_click_tool/MVP/main.cpp
@@ -1,6 +1,6 @@
 #define GL_SILENCE_DEPRECATION
 
-// #include <glad/glad.h>
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -32,8 +32,8 @@ int main(int argc, char** argv) {
     }
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); 
-
-  /*  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+/*
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         return -1;
     }*/
 
